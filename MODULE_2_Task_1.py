37da87d16735c84dd653d56fd2706abc6a7a9a8a
mdsfhusn <mdsfhusn@gmail.com>
2025-08-26 21:51:15 +0530

    Create MODULE_2_Task_1.py
    assignment completion

---
+++ MODULE_2_Task_1.py
@@ -0,0 +1,11 @@
+"""This file is for the TuteDude MODULE 2 ASSIGNMENT"""
+num1 = input("Enter the first number : ")
+num2 = input("Enter the second number : ")
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print("Addition : ",add)
+print("Subtraction : ",sub)
+print("Multiplication : ",mul)
+print("Division : ",div)
